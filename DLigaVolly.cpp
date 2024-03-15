f21dd3eee61464b9340fca65e9a22704f2453342
de'all <137359388+Alizaaaja4@users.noreply.github.com>
2024-03-15 14:53:19 +0700

    First Commits

---
+++ DLigaVolly.cpp
@@ -0,0 +1,116 @@
+/*
+Fitur program (file.txt)
+-> Admin
+- login username dan pass
+- add daftar tim
+- melihat daftar tim
+- update informasi tim
+- add jadwal pertandingan
+- delate jadwal pertandingan
+- delate tim
+
+-> Public
+- searching tim (untuk informasi)
+- lihat jadwal pertandingan tim
+- melihat ranking klasmen tim
+
+*/
+
+#include <iostream>
+#include <unistd.h>
+#include <cstring>
+using namespace std;
+
+void baper();
+
+void log_admin();
+void dash_admin();
+void add_team();
+void view_team();
+void delate_team();
+void upd_team();
+void add_jadwal_team();
+void delate_jadwal_team();
+
+void dash1_public();
+void regis_public();
+void log_public();
+void view_jadwal_public();
+void view_rank_public();
+void search_public();
+
+struct items{
+    char uname[30], pass[30];
+    string cari;
+}; items its;
+
+void dash_admin(){
+    string menu;
+    cout << "\033[2J\033[1;1H";
+    cout << "-------------------------- D'LIGAVOLLY ------------------------" << endl;
+    cout << "---------------------- MENU ADMINISTRATOR ---------------------" << endl;
+    cout << "---------------------------------------------------------------" << endl;
+    cout << "----------           [A]. Input Data Team            ----------" << endl;
+    cout << "----------           [B]. Review Data Team           ----------" << endl;
+    cout << "----------           [C]. Delate Data Team           ----------" << endl;
+    cout << "----------           [D]. Update Data Team           ----------" << endl;
+    cout << "----------           [E]. Input Jadwal               ----------" << endl;
+    cout << "----------           [F]. Review Jadwal              ----------" << endl;
+    cout << "----------           [G]. Delate Jadwal              ----------" << endl;
+    cout << "----------           [H]. Update Jadwal              ----------" << endl;
+    cout << "---------------------------------------------------------------" << endl;
+    cout << "----------           [Z]. Logout                     ----------" << endl;
+    cout << "---------------------------------------------------------------" << endl;
+    cout << "---------                Input Option                ----------" << endl;
+    cout << "                            /> "; cin >> menu;
+    cout << "---------------------------------------------------------------" << endl;
+}
+
+void log_admin(){
+    cout << "-------------------------- D'LIGAVOLLY ------------------------" << endl;
+    cout << " -------------------- LOGIN ADMINISTRATOR ---------------------" << endl;
+    cout << "---------------------------------------------------------------" << endl;
+    cout << "\t\t     Username: "; cin >> its.uname;
+    cout << "\t\t     Password: "; cin >> its.pass;
+    cout << "---------------------------------------------------------------" << endl << endl;
+    cout << "\t\t\tProses Verifikasi...." << endl; sleep(3);
+
+    if(strcmp(its.uname, "vollydaskom")== 0 && strcmp(its.pass, "2424")==0){
+        cout << "\t\t\t  Login Success!!" << endl; sleep(3); dash_admin();
+    } else {
+        cout << "\t\t\t  Login failed!!" << endl; sleep(3); log_admin();
+    }
+}
+
+void baper(){
+    cout << "-------------------------- D'LIGAVOLLY ------------------------" << endl;
+    cout << " --- A simple practice project to deepen your understanding ---" << endl;
+    cout << " --- of C++ programming by building a user-friendly app     ---" << endl;
+    cout << "---------------------------------------------------------------" << endl;
+}
+
+int main (){
+    int menu;
+
+    cout << "\033[2J\033[1;1H";
+    cout << endl;
+    baper();
+    cout << ("\t\t       [1]. D'Liga Admin") << endl;
+    cout << ("\t\t       [2]. D'Liga Public") << endl;
+    cout << "---------------------------------------------------------------" << endl;
+    cout << "\t\t\t    /> "; cin >> menu;
+
+    switch(menu){
+        case 1: cout << "\033[2J\033[1;1H";
+                log_admin(); break;
+        
+        case 2: cout << "\033[2J\033[1;1H";
+                break;
+        
+        default: cout << "\033[2J\033[1;1H";
+             cout << endl << "Maaf option ada tidak terdekteksi!!" << endl; sleep(3);
+             main(); 
+    }
+    return 0;
+}
+
